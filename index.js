23f06e49f897009e2d588452b668fc913c95a38f
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-08-06 08:04:12 +0100

    update port

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const url = "https://api.nasa.gov/planetary/apod?api_key=";
 const api_key = "6F5oevHn2pzNGHpLgWR32PrbR2MIn61GdnryrxeP";
 let nasaImage = ''
 const app = express()
+const port = process.env.PORT || 8080
 
 const fetchData = async () => {
 	try {
@@ -27,7 +28,7 @@ app.get('/', (req, res) => {
 	res.redirect(nasaImage.toString())
 })
 
-app.listen(8080, (req, res) => {
-	console.log('running at port 8080');
+app.listen(port, (req, res) => {
+	console.log('running at port ' + port);
 })
 // console.log('nasa image' + nasaImage)
